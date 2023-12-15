dc6c41bba41df9d69c4ac5c3ecf37accf44e4cb3
ImKingEz <angpao29@gmail.com>
2023-12-15 17:05:06 +0100

    add get e set frasi e pagina

--- src/main/java/dao/ListinoDAO.java
+++ src/main/java/dao/ListinoDAO.java
@@ -4,13 +4,22 @@ import controller.GiaEsistenteException;
 import controller.NotABlankException;
 import model.*;
 
+import java.util.ArrayList;
+
 public interface ListinoDAO {
 
     public void setUtente(String username, String password) throws Exception;
-    public void setSchema();
     public Utente getUtente(String username);
+
+    public void setPagina(String titolo, Utente autore) throws GiaEsistenteException, NotABlankException;
+    public Pagina getPagina(String titolo);
+    public int getIdPagina(String titolo) throws ClassNotFoundException;
+
+    public void setFrase(String testo, Pagina pagina) throws NotABlankException;
+    public ArrayList<Frase> getFrasi(Pagina pagina);
+
+    public void setSchema();
     public boolean checkEsistenzaUtenti();
     public boolean checkEsistenzaPagine();
-    public void setPagina(String titolo, Utente autore) throws GiaEsistenteException, NotABlankException;
     public int numeroPagineCreateDaUnUtente(String username);
 }
